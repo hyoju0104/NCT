78e0401545aa1cf4ab101265673eb5786d0c0f00
dlkara <lhj020731@gmail.com>
2025-05-12 10:45:28 +0900

    Post list 페이지 생성

--- src/main/java/com/lec/spring/NctApplication.java
+++ src/main/java/com/lec/spring/NctApplication.java
@@ -1,8 +1,10 @@
 package com.lec.spring;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.lec.spring.repository")
 @SpringBootApplication
 public class NctApplication {
 
--- src/main/java/com/lec/spring/controller/PostController.java
+++ src/main/java/com/lec/spring/controller/PostController.java
@@ -1,10 +1,35 @@
 package com.lec.spring.controller;
 
+import com.lec.spring.domain.Post;
+import com.lec.spring.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/post")
 public class PostController {
-
-
+	
+	private final PostService postService;
+	
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
+	
+	@RequestMapping("/list")
+	public void list(Model model) {
+		List<Post> posts = postService.list();
+		System.out.println("조회된 게시글 수: " + (posts != null ? posts.size() : 0));
+		if (posts != null && !posts.isEmpty()) {
+			for (Post post : posts) {
+				System.out.println("Post ID: " + post.getId() +
+						", Content: " + post.getContent() +
+						", User: " + (post.getUser() != null ? post.getUser().getUsername() : "null"));
+			}
+		}
+		model.addAttribute("posts", posts);
+	}
 
 }
