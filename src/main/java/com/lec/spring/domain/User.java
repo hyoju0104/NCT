490bf54b789e4ada9be851718115972e07ea8fc3
hyoju0104 <hyoju8618@naver.com>
2025-05-13 16:54:58 +0900

    로그인 , 회원가입 끝

--- src/main/java/com/lec/spring/domain/User.java
+++ src/main/java/com/lec/spring/domain/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -44,5 +45,4 @@ public class User {
 	
 	private Integer rentalCnt;
 
-	
 }
