a5c85dcb98b7b34f691317ca55287e0519d12650
dlkara <lhj020731@gmail.com>
2025-05-13 14:46:57 +0900

    item order detail : user 정보 가져오기 성공

--- src/main/java/com/lec/spring/controller/OrderController.java
+++ src/main/java/com/lec/spring/controller/OrderController.java
@@ -1,14 +1,17 @@
 package com.lec.spring.controller;
 
+import com.lec.spring.config.PrincipalDetails;
 import com.lec.spring.domain.Item;
 import com.lec.spring.domain.User;
 import com.lec.spring.service.ItemService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/order")
@@ -21,9 +24,17 @@ public class OrderController {
     }
 
     @GetMapping("/detail/{id}")
-    public String orderDetail(@PathVariable Long id, Model model, HttpSession session) {
+    public String orderDetail(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes, HttpSession session, @AuthenticationPrincipal PrincipalDetails principal) {
         Item item = itemService.detail(id);
-        User user = (User) session.getAttribute("user");
+//        User user = (User) session.getAttribute("user");
+        
+        // 1) 로그인 체크
+        if (principal == null) {
+            redirectAttributes.addFlashAttribute("error", "로그인 후 작성 가능합니다.");
+            return "redirect:/post/list";
+        }
+        // 2) Post 에 User 주입
+        User user = principal.getUser();
 
         model.addAttribute("item", item);
         model.addAttribute("user", user);
