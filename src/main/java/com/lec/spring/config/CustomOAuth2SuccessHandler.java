6e517f9e5893b69a1c2bef8581a06ab1b4aaf62a
dlkara <lhj020731@gmail.com>
2025-05-16 17:42:49 +0900

    handler 에서 oauth 로그인 및 회원가입 시 추가 정보 입력 리다이렉트 부분 삭제

--- src/main/java/com/lec/spring/config/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/lec/spring/config/CustomOAuth2SuccessHandler.java
@@ -32,22 +32,7 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
             log.warn("응답이 이미 커밋되어 리다이렉트할 수 없습니다.");
             return;
         }
-
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        String provider = ((PrincipalUserDetails) oAuth2User).getUser().getProvider();
-        User user = ((PrincipalUserDetails) oAuth2User).getUser();
-
-        System.out.println("🟢 OAuth2 인증 성공, 리다이렉트 시작 전");
-        if (user.getPhoneNum() == null || user.getPhoneNum().isBlank()) {
-            log.info("▶️ 추가 정보 입력이 필요함: phone_num이 없음");
-
-            // 임시로 세션에 사용자 ID 저장 (추가정보 입력 후 처리용)
-            request.getSession().setAttribute("userIdForAdditionalInfo", user.getId());
-
-            // 추가정보 입력 페이지로 리다이렉트
-            response.sendRedirect("/register/user");
-        }
-
+        
         response.sendRedirect("/post/list");
     }
 
