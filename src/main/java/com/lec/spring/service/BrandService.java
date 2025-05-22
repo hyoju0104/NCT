083cfe79e69cb9d4406d7fe122c12bb705bd4c97
haehoon <hoon970413@gmail.com>
2025-05-22 17:30:34 +0900

    최해훈의 주석달기 1차

--- src/main/java/com/lec/spring/service/BrandService.java
+++ src/main/java/com/lec/spring/service/BrandService.java
@@ -8,21 +8,28 @@ import java.util.List;
 
 public interface BrandService {
 
+    // 브랜드 회원가입
     int register(Brand brand);
 
+    // username 중복 확인
     boolean isExist(String username);
 
+    // username 조회
     Brand findByUsername(String username);
 
+    // 권한 조회
     List<Authority> selectAuthoritiesById(Long id);
 
-
+    // 정보 조회
     Brand myDetail(Long id);
 
+    // id 조회
     Brand selectById(Long id);
 
+    // 정보 수정
     int myUpdate(Brand brand);
 
+    // 계정 탈퇴
     int myDelete(Long id);
 
 }
