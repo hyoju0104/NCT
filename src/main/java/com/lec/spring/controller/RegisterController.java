6a8aa6f29e48030bcb5c28dd542e46e214214f17
dlkara <lhj020731@gmail.com>
2025-05-26 16:26:55 +0900

    brand 회원가입 경로 수정

--- src/main/java/com/lec/spring/controller/RegisterController.java
+++ src/main/java/com/lec/spring/controller/RegisterController.java
@@ -95,23 +95,23 @@ public class RegisterController {
             @ModelAttribute Brand brand, // form에서 입력한 이름, 아이디, 비번 등을 자동으로 Brand 객체에 담아줌. 예: <input name="username"> → brand.getUsername()
             @RequestParam("logo") MultipartFile logo, //사용자가 첨부한 로고 이미지 파일을 logoFile이라는 변수로 받음. <input type="file" name="logo"> 이거 ㅇㅇ
             HttpServletRequest request, //사용자가 보낸 모든 요청 직접 확인할 수 있는 객체(rePassword 값 꺼내쓰려고)
-            RedirectAttributes redirectAttrs //회원가입 실패 시 다시 회원가입 페이지로 돌아갈 때 이유를 같이 보내기 위해 사용
+            RedirectAttributes redirectAttributes //회원가입 실패 시 다시 회원가입 페이지로 돌아갈 때 이유를 같이 보내기 위해 사용
     ) {
         // 1. 비밀번호 일치 확인
         String rePassword = request.getParameter("rePassword");
         if (!brand.getPassword().equals(rePassword)) {
-            redirectAttrs.addAttribute("error", "password");
+            redirectAttributes.addAttribute("error", "password");
             return "redirect:/register/brand";
         }
 
         // 2. 아이디 중복 확인
         if (brandService.isExist(brand.getUsername())) {
-            redirectAttrs.addAttribute("error", "exist");
+            redirectAttributes.addAttribute("error", "exist");
             return "redirect:/register/brand";
         }
 
         // 3. 회원가입 먼저 수행 (brand.id가 생성됨)
-        brandService.register(brand);
+        redirectAttributes.addFlashAttribute("result", brandService.register(brand));
 
         // 4. 첨부파일 저장 (로고 업로드)
         if (logo != null && !logo.isEmpty()) {
@@ -126,7 +126,7 @@ public class RegisterController {
                 }
                 logo.transferTo(newFile);
             } catch (IOException e) {
-                redirectAttrs.addFlashAttribute("error", "파일 저장 중 오류가 발생했습니다.");
+                redirectAttributes.addFlashAttribute("error", "파일 저장 중 오류가 발생했습니다.");
                 return "redirect:/register/brand";
             }
 
@@ -137,9 +137,8 @@ public class RegisterController {
 
             brandAttachmentService.save(attachment);
         }
-
-
-        return "redirect:/login";
+        
+        return "redirect:/register/registerOk";
     }
 
 }
