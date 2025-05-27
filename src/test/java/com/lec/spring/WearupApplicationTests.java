af52a6ab45037af39778121bfcd7b91ecca31a24
dlkara <lhj020731@gmail.com>
2025-05-27 22:40:46 +0900

    OAuth2 test 용 더미 빈 생성

--- src/test/java/com/lec/spring/WearupApplicationTests.java
+++ src/test/java/com/lec/spring/WearupApplicationTests.java
@@ -2,12 +2,32 @@ package com.lec.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @SpringBootTest
 class WearupApplicationTests {
+    
+    static class OAuth2TestConfig {
+        // OAuth2 client repository dummy bean
+        @Bean
+        public ClientRegistrationRepository clientRegistrationRepository() {
+            return new InMemoryClientRegistrationRepository();
+        }
+        
+        // OAuth2 authentication client storage dummy bean
+        @Bean
+        public OAuth2AuthorizedClientService authorizedClientService(ClientRegistrationRepository registrations) {
+            return new InMemoryOAuth2AuthorizedClientService(registrations);
+        }
+    }
 
     @Test
     void contextLoads() {
+        // 컨텍스트 로드만 검증
     }
 
 }
